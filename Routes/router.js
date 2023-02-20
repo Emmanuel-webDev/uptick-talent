e90f9a01f9dfe7bcb00d93b4e0c56836770c8d7c
Emmanuel Onuorah <emmanuelonuorah875@gmail.com>
2023-02-20 13:20:13 +0100

    Added the read feature

--- Routes/router.js
+++ Routes/router.js
@@ -12,4 +12,24 @@ app.post('/createNote', async(req, res)=>{
    res.status(201).send('Note created successfully')
 })
 
+//Get all notes
+app.get('/note', async(req, res)=>{
+    const notes = await noteDB.find()
+
+    if(!notes){
+        res.status(404).send("No notes yet!!")
+    }
+    res.send(notes)
+})
+
+//Get a particular note
+app.get('/note/:id', async(req, res)=>{
+    const note = await noteDB.findById({_id: req.params.id})
+
+    if(!notes){
+        res.status(404).send("Note not found")
+    }
+    res.send(note)
+})
+
 module.exports = app
